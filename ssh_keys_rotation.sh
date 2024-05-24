d57548fc043c3e50e39eb3ece9dfb2cf5d9e2af9
AbedAwaisy <abed.awaisy.a@gmail.com>
2024-05-25 02:33:36 +0300

    remove old keys; rotation script

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -28,7 +28,7 @@ NEW_KEY_PATH="/home/ubuntu/new_key"
 ssh-keygen -t rsa -b 4096 -f $NEW_KEY_PATH -N "" -C "key_rotation"
 
 # Append the new public key to authorized_keys on the private instance
-cat $NEW_KEY_PATH.pub | ssh -i "$KEY_PATH" ubuntu@$PRIVATE_IP "cat >> ~/.ssh/authorized_keys"
+cat $NEW_KEY_PATH.pub | ssh -i "$KEY_PATH" ubuntu@$PRIVATE_IP "cat > ~/.ssh/authorized_keys"
 
 # Replace the old key with the new key in the KEY_PATH
 sudo mv $NEW_KEY_PATH $KEY_PATH
